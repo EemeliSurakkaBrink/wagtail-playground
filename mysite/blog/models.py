3e118ad3f70df3abde4f8eec45c8adf556afbc3c
Eemeli Surakka <eemeli@thebrinkagency.com>
2025-09-02 16:48:20 +0200

    feat: fetch pages using context

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,9 +16,19 @@ class BlogIndexPage(Page):
 
 
 class BlogPage(Page):
+    
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
+    
+    # add the get_context method:
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
