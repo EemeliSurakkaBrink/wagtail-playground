3fd785cab46b39c51632943029852e8ed1d0bbae
Eemeli Surakka <eemeli@thebrinkagency.com>
2025-09-02 13:50:33 +0200

    init

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wv91=yza@f-x9p5qh$)7w-q+fz*4q=rcx*))lnze71ra8sxb7_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
